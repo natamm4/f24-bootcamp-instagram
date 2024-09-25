35f3a543ded8dfe212fc38921ddd39b69eac94dd
natamm <natamm@unc.edu>
2024-09-25 15:43:17 -0400

    1 million and one likes

--- instagram/instagram/PostView.swift
+++ instagram/instagram/PostView.swift
@@ -8,28 +8,44 @@
 import SwiftUI
 
 struct PostView: View {
+    @State private var isLiked: Bool = false
+    @State private var isBookmarked: Bool = false
+    //@State private var likes: String = "1 million"
+    //@State private var likesAndOne: String = "1 million and one"
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 11) {
             HStack {
                 Image(.adele).resizable().scaledToFit().frame(width: 30).clipShape(Circle())
                 Text("constructiontwerker47").font(.subheadline).bold()
                 Spacer()
-                Image(systemName: "ellipsis")
+                Button() {
+                } label: {
+                    Image(systemName: "ellipsis").foregroundStyle(.black)
+                }
             }
             .padding(.horizontal, 8)
             Image(.butterflie).resizable().scaledToFit()
             HStack(spacing: 16) {
-                Image(systemName: "heart")
+                Button(action: {
+                    self.isLiked.toggle()
+                }) {
+                    self.isLiked == true ? Image(systemName: "heart.fill").foregroundStyle(.red) : Image(systemName: "heart").foregroundStyle(.black)
+                }
                 Image(systemName: "message")
                 Image(systemName: "paperplane")
                 Spacer()
-                Image(systemName: "bookmark")
+                Button(action: {
+                    self.isBookmarked.toggle()
+                }) {
+                    self.isBookmarked == true ? Image(systemName:"bookmark.fill").foregroundStyle(.black) : Image(systemName: "bookmark").foregroundStyle(.black)
+                }
             }
             .font(.title2)
             .padding(.horizontal, 8)
             VStack() {
                 HStack {
-                    Text("1 million likes").font(.footnote).fontWeight(.semibold)
+                    Text(self.isLiked == true ? "1 million and one likes" : "1 million likes").font(.footnote).fontWeight(.semibold)
                     Spacer()
                 }
                 .padding(.horizontal, 8)
